123fefdabe8c235edc8f045d51c7c044ca8f93e5
Mary Dick <maryclairedick@gmail.com>
2025-06-10 12:55:06 -0500

    add routes for signup/login/logout

--- src/App.js
+++ src/App.js
@@ -3,18 +3,40 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './pages/Home';
 import Layout from './pages/NavBar';
 import SignUp from './pages/SignUp';
-
-function App() {
-  return (
-   <BrowserRouter>
-      <Routes>
-        <Route path="/"element={<Layout/>}>
-          <Route index element={<Home/>} />
-          <Route path="signup" element={<SignUp/>}/>
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
+  render() {
+    return (
+     <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={<Layout handleLogout={this.handleLogout} loggedInStatus={this.state.isLoggedIn}/>}
+          >
+            <Route index element={<Home/>} />
+            <Route
+              path="/signup"
+              element={
+                <SignUp
+                  history={this.props.history}
+                  handleLogin={this.handleLogin}
+                  loggedInStatus={this.state.isLoggedIn}
+                />
+              }
+            />
+            <Route
+              exact path="/login"
+              element={
+                <LogIn
+                  history={this.props.history}
+                  handleLogin={this.handleLogin}
+                  loggedInStatus={this.state.isLoggedIn}
+                />
+              }
+            />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
