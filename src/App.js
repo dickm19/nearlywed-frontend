2edd85978bab5f2ae5cb24046d5fd71a008dfdef
Mary Dick <maryclairedick@gmail.com>
2025-06-10 12:55:39 -0500

    add helper functions for handling login/logout:

--- src/App.js
+++ src/App.js
@@ -1,8 +1,51 @@
 import './App.css';
+import React, { Component } from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import axios from 'axios';
 import Home from './pages/Home';
 import Layout from './pages/NavBar';
 import SignUp from './pages/SignUp';
+import LogIn from './pages/LogIn';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      isLoggedIn: false,
+      user: {}
+     };
+  }
+  handleLogin = (data) => {
+    this.setState({
+      isLoggedIn: true,
+      user: data.user
+    })
+  }
+
+  handleLogout = () => {
+    this.setState({
+      isLoggedIn: false,
+      user: {}
+    })
+  }
+
+  loginStatus = () => {
+    axios.get('http://localhost:3001/logged_in', 
+    {withCredentials: true})
+    .then(response => {
+      if (response.data.logged_in) {
+        this.handleLogin(response)
+      } else {
+        this.handleLogout()
+      }
+    })
+    .catch(error => console.log('api errors:', error))
+  }
+
+  componentDidMount() {
+    this.loginStatus()
+  }
+
   render() {
     return (
      <BrowserRouter>
