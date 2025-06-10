c70d826e0305a28afcf62beb89ac231e0a49e14f
Mary Dick <maryclairedick@gmail.com>
2025-06-10 12:58:19 -0500

    use class component instead of functional

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,12 @@
-export default function Home() {
-    return (
-        <div className="home">
-            <h1>Welcome to the Home Page</h1>
-        </div>
-    )
+import { Component } from 'react';
+class Home extends Component {
+    render() {
+        return (
+            <div className="home">
+                <h1>Welcome to the Home Page</h1>
+            </div>
+        )
+    }
 }
+
+export default Home;
