22fe076fda12a7598b6f9857d87563d5779988e4
Mary Dick <maryclairedick@gmail.com>
2025-06-12 13:50:31 -0500

    convert Profile class component into a functional component

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,61 +1,55 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 import image from '../assets/defaultProfile.png';
 
-class Profile extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            profilePicture: props.user.avatar_url || image
-        };
-    }
+function Profile({ user }) {
+    const [profilePicture, setProfilePicture] = useState(user.avatar_url || image);
 
-    componentWillReceiveProps(nextProps) {
-        console.log('nextProps', nextProps);
-        this.setState({
-            profilePicture: nextProps.user.avatar_url || image
-        });
-    }
+    useEffect(() => {
+        setProfilePicture(user.avatar_url || image);
+    }, [user]);
 
-
-    handleImageChange = (event) => {
+    const handleImageChange = (event) => {
         const file = event.target.files[0];
         if (file) {
-            const formData = { user: { avatar: file } };
+            const formData = new FormData();
+            formData.append('user[avatar]', file);
 
-            axios.patch(`http://localhost:3001/users/${this.props.user.id}`, formData, {
+            axios.patch(`http://localhost:3001/users/${user.id}`, formData, {
                 withCredentials: true,
                 headers: {
                     'Content-Type': 'multipart/form-data'
                 }
             })
             .then(response => {
-                this.setState({ profilePicture: response.data.avatar_url });
+                setProfilePicture(response.data.avatar_url);
             })
             .catch(error => console.log('Error uploading image:', error));
         }
-    }
+    };
 
-    render() {
-        return (
-            <div className="profile">
-                <h1>Profile Page</h1>
-                <div className="profile-image-section">
-                    <img
-                        className="profile-picture"
-                        src={this.state.profilePicture}
-                        alt="Profile"
-                    />
-                    <label htmlFor="image-upload" className="image-upload-label">
-                        Update Profile Photo
-                    </label>
-                    <input type="file" accept="image/*" onChange={this.handleImageChange} />
-                </div>
-                <p>Email: {this.props.user.email}</p>
-                
+    return (
+        <div className="profile">
+            <h1>Profile Page</h1>
+            <div className="profile-image-section">
+                <img
+                    className="profile-picture"
+                    src={profilePicture}
+                    alt="Profile"
+                />
+                <label htmlFor="image-upload" className="image-upload-label">
+                    Update Profile Photo
+                </label>
+                <input
+                    id="image-upload"
+                    type="file"
+                    accept="image/*"
+                    onChange={handleImageChange}
+                />
             </div>
-        );
-    }
+            <p>Email: {user.email}</p>
+        </div>
+    );
 }
 
 export default Profile;
