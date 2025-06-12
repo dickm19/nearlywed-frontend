5cfd7be79abdf822a31ee9b5eb302c7fe7f709d2
Mary Dick <maryclairedick@gmail.com>
2025-06-12 14:54:12 -0500

    use react-hook-form to allow profile edits

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,12 +1,29 @@
 import { useState, useEffect } from 'react';
+import { useForm } from 'react-hook-form';
 import axios from 'axios';
 import image from '../assets/defaultProfile.png';
 
-function Profile({ user }) {
-    const [profilePicture, setProfilePicture] = useState(user.avatar_url || image);
+function Profile(props) {
+    const { user, handleUserUpdate } = props;
+    const [profilePicture, setProfilePicture] = useState(image);
+    const [editMode, setEditMode] = useState(false);
+    const [email, setEmail] = useState('');
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+
+    const { register, handleSubmit } = useForm({
+        values: {
+            email: user.email || email,
+            first_name: user.first_name || firstName,
+            last_name: user.last_name || lastName
+        }
+    });
 
     useEffect(() => {
         setProfilePicture(user.avatar_url || image);
+        setEmail(user.email || '');
+        setFirstName(user.first_name || '');
+        setLastName(user.last_name || '');
     }, [user]);
 
     const handleImageChange = (event) => {
@@ -28,9 +45,24 @@ function Profile({ user }) {
         }
     };
 
+    const onSubmit = (data) => {
+        const { email, first_name: firstName, last_name: lastName } = data;
+        const updatedUser = { email, first_name: firstName, last_name: lastName };
+
+        axios.patch(`http://localhost:3001/users/${user.id}`, { user: updatedUser }, { withCredentials: true })
+            .then(response => {
+                setEditMode(false);
+                handleUserUpdate({ user: response.data });
+                setEmail(response.data.email);
+                setFirstName(response.data.first_name);
+                setLastName(response.data.last_name);
+            })
+            .catch(error => console.log('Error updating profile:', error));
+    }
+
     return (
         <div className="profile">
-            <h1>Profile Page</h1>
+            <button className="edit-profile-button" onClick={() => setEditMode(true)}>Edit Profile</button>
             <div className="profile-image-section">
                 <img
                     className="profile-picture"
@@ -47,7 +79,30 @@ function Profile({ user }) {
                     onChange={handleImageChange}
                 />
             </div>
-            <p>Email: {user.email}</p>
+            <div className="profile-info">
+                {editMode ? (
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <div className="form-group"></div>
+                            <label htmlFor='email'>Email</label>
+                            <input {...register('email', { required: true, pattern: /^\S+@\S+$/i })}/>
+                        <div/>
+                        <div className="form-group"></div>
+                            <label htmlFor='first_name'>First Name</label>
+                            <input {...register('first_name')} />
+                        <div/>
+                        <div className="form-group"></div>
+                            <label htmlFor='last_name'>Last Name</label>
+                            <input {...register('last_name')}/>
+                        <div/>
+                        <button type="submit">Save</button>
+                    </form>
+                ) : (
+                    <>
+                        <p>Email: {email}</p>
+                        <p>Name: {`${firstName} ${lastName}`}</p>
+                    </>
+                )}
+            </div>
         </div>
     );
 }
