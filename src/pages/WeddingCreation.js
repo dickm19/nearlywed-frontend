de6975c88349b99aeb67e187fed94e6407c217f7
Mary Dick <maryclairedick@gmail.com>
2025-06-13 13:17:56 -0500

    add popup component for creating a wedding

---
+++ src/pages/WeddingCreation.js
@@ -0,0 +1,10 @@
+function WeddingCreation() {
+    return (
+        <div>
+            <h1>Wedding Creation Page</h1>
+            <p>This is where nearlyweds can create their wedding registry and manage wedding details.</p>
+        </div>
+    )
+}
+
+export default WeddingCreation;
