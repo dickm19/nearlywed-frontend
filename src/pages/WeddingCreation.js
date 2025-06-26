6e186ee18a947c3f05fba9ac8fec627ea04a9e9d
Mary Dick <maryclairedick@gmail.com>
2025-06-26 16:02:32 -0500

    fix indentation and add 'incomplete feature' note

--- src/pages/WeddingCreation.js
+++ src/pages/WeddingCreation.js
@@ -5,10 +5,11 @@ function WeddingCreation({ user }) {
     return (
         <div className="wedding-creation">
             <h3>Create your Registry</h3>
-                <p>this feature is incomplete</p>
+            <p>this feature is incomplete</p>
             <h3>Invite Guests</h3>
                 <EmailInput submitText="Invite Guests"/>
             <h3>Manage Wedding Details</h3>
+            <p>this feature is incomplete</p>
         </div>
     )
 }
