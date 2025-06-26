fde381c32d43fbfb3a68ce415d3d1a9b5b140a62
Mary Dick <maryclairedick@gmail.com>
2025-06-26 16:35:04 -0500

    Add nav for wedding creation

--- src/pages/WeddingCreation.js
+++ src/pages/WeddingCreation.js
@@ -1,15 +1,37 @@
 import '../styles/WeddingCreation.scss';
 import EmailInput from './Fields/EmailInput';
+import { Link } from 'react-router-dom';
+import { useState } from 'react'
 
 function WeddingCreation({ user }) {
+    const [selected, setSelected] = useState('home');
+
+    const handleClick = (event) => {
+        setSelected(event);
+    }
+
     return (
         <div className="wedding-creation">
-            <h3>Create your Registry</h3>
-            <p>this feature is incomplete</p>
-            <h3>Invite Guests</h3>
-                <EmailInput submitText="Invite Guests"/>
-            <h3>Manage Wedding Details</h3>
-            <p>this feature is incomplete</p>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="#" onClick={() => handleClick('home')}>Home</Link>
+                    </li>
+                    <li>
+                        <Link to="#" onClick={() => handleClick('registry')}>Create your Registry</Link>
+                    </li>
+                    <li>
+                        <Link to="#" onClick={() => handleClick('guests')}>Invite/Manage Guests</Link>
+                    </li>
+                    <li>
+                        <Link to="#" onClick={() => handleClick('details')}>Manage Wedding Details</Link>
+                    </li>
+                </ul>
+            </nav>
+            { selected === 'home' ? <p>Home test</p> : null }
+            { selected === 'registry' ? <p>Registry test</p> : null}
+            { selected === 'guests' ? <EmailInput submitText="Invite Guests"/> : null }
+            { selected === 'details' ? <p>Details test</p> : null }
         </div>
     )
 }
