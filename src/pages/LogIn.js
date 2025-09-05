dd2bbb690dbad1e58a6123f363e65d35fe72c6dc
Mary Dick <maryclairedick@gmail.com>
2025-09-05 13:28:55 -0500

    import login scss file

--- src/pages/LogIn.js
+++ src/pages/LogIn.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { Navigate } from 'react-router-dom';
 import axios from 'axios';
+import '../styles/Login.scss';
 
 function LogIn(props) {
     const [email, setEmail] = useState('');
